eef8197ea15344bfa129fb5f6d92212dbae38f93
Kalleb <Kallebpradosi@gmail.com>
2024-03-01 22:26:37 -0300

    Projeto com padrão

--- arquivosTestes/src/StringInvertida.java
+++ arquivosTestes/src/StringInvertida.java
@@ -7,11 +7,8 @@ public class StringInvertida {
         System.out.println("Digite uma palavra");
         String p = sc.nextLine();
         int t = p.length();
-        char palavraInvertida[] = new char[t];
         for (int i = p.length() - 1; i > -1; i--) {
-            int ind = 0;
-            System.out.print(palavraInvertida[ind] = p.charAt(i));
-            ind++;
+            System.out.print(p.charAt(i));
         }
 
     }
